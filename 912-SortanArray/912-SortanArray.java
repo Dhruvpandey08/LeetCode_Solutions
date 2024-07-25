4dd96d55d600823b42e62448db2c98556c2c21ec
Dhruv Pandey <dhruvpandey928@gmail.com>
2024-07-25 18:38:04 +0530

    Create file

---
+++ 912-SortanArray/912-SortanArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+    }
+        Arrays.sort(nums);
+        return nums;
+}
+[
+[5,2,3,1]
+[5,1,1,2,0,0]
+[1,2,3,5]
+[0,0,1,1,2,5]
+[1,2,3,5]
+[0,0,1,1,2,5]
