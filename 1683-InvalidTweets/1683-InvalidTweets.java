a3340746fc454bbfb4650b58276385da6b4643c5
Dhruv Pandey <dhruvpandey928@gmail.com>
2024-07-26 10:08:12 +0530

    Create file

---
+++ 1683-InvalidTweets/1683-InvalidTweets.java
@@ -0,0 +1,10 @@
+select tweet_id from Tweets where length(content)>15;
+[
+{"headers":{"Tweets":["tweet_id","content"]},"rows":{"Tweets":[[1,"Vote 
+for Biden"],[2,"Let us make America great again!"]]}}
+| tweet_id |
+| -------- |
+| 2        |
+| tweet_id |
+| -------- |
+| 2        |
