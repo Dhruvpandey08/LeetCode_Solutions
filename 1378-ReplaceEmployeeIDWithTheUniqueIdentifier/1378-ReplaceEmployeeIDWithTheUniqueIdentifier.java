be29eab0c66a6d2d69d579360fe6d8afd74c7ae8
Dhruv Pandey <dhruvpandey928@gmail.com>
2024-07-27 23:03:50 +0530

    Create file

---
+++ 1378-ReplaceEmployeeIDWithTheUniqueIdentifier/1378-ReplaceEmployeeIDWithTheUniqueIdentifier.java
@@ -0,0 +1,19 @@
+select unique_id, name from Employees left JOIN EmployeeUNI USING(id);
+[
+{"headers":{"Employees":["id","name"],"EmployeeUNI":["id","unique_id"]},
+"rows":{"Employees":[[1,"Alice"],[7,"Bob"],[11,"Meir"],[90,"Winston"],[3,
+"Jonathan"]],"EmployeeUNI":[[3,1],[11,2],[90,3]]}}
+| unique_id | name     |
+| --------- | -------- |
+| null      | Alice    |
+| null      | Bob      |
+| 2         | Meir     |
+| 3         | Winston  |
+| 1         | Jonathan |
+| unique_id | name     |
+| --------- | -------- |
+| null      | Alice    |
+| null      | Bob      |
+| 2         | Meir     |
+| 3         | Winston  |
+| 1         | Jonathan |
