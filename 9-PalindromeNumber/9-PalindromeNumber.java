38a212a9ef67a297a717a76deb450e4dde654f33
Dhruv Pandey <dhruvpandey928@gmail.com>
2024-07-30 23:48:36 +0530

    Create file

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,19 @@
+            reverse = (reverse * 10) + (x%10);
+        {
+        if(x<0)
+            return false;
+        while(x != 0)
+    public boolean isPalindrome(int x) {
+        int reverse = 0;
+        int temp = x;
+class Solution {
+1
+121
+-121
+10
+true
+false
+false
+true
+false
+false
