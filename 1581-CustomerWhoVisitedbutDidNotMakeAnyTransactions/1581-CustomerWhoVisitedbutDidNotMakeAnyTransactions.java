a9ac3f9a080b43d566787e899a0ab45ab4b42ce5
Dhruv Pandey <dhruvpandey928@gmail.com>
2024-07-31 23:58:44 +0530

    Create file

---
+++ 1581-CustomerWhoVisitedbutDidNotMakeAnyTransactions/1581-CustomerWhoVisitedbutDidNotMakeAnyTransactions.java
@@ -0,0 +1,19 @@
+SELECT customer_id, COUNT(*) AS count_no_trans
+FROM Visits
+WHERE visit_id NOT IN (SELECT visit_id FROM Transactions)
+GROUP BY customer_id;
+[
+{"headers":{"Visits":["visit_id","customer_id"],"Transactions":
+["transaction_id","visit_id","amount"]},"rows":{"Visits":[[1,23],[2,9],[4,
+30],[5,54],[6,96],[7,54],[8,54]],"Transactions":[[2,5,310],[3,5,300],[9,5,
+200],[12,1,910],[13,2,970]]}}
+| customer_id | count_no_trans |
+| ----------- | -------------- |
+| 30          | 1              |
+| 96          | 1              |
+| 54          | 2              |
+| customer_id | count_no_trans |
+| ----------- | -------------- |
+| 30          | 1              |
+| 96          | 1              |
+| 54          | 2              |
